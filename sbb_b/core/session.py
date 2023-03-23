741a0a89998bd0e30e846f25fcf7054960096e16
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-03-23 16:00:20 +0200

    Update session.py

--- sbb_b/core/session.py
+++ sbb_b/core/session.py
@@ -7,7 +7,7 @@ from .bothseesion import bothseesion
 from .client import JmthonClient
 from .logger import logging
 
-LOGS = logging.getLogger("سورس سيمو")
+LOGS = logging.getLogger("سورس افاتار")
 __version__ = "2.10.6"
 
 loop = None
